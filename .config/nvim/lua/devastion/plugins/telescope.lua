f256dd34d0036eb94e64a523f0fb8fe3231518e9
Dimitar Banev <banev_dimitar@mail.com>
2024-04-30 02:01:41 +0300

    added which-key and telescome to nvim config

---
+++ .config/nvim/lua/devastion/plugins/telescope.lua
@@ -0,0 +1,71 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.6",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+		"nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local builtin = require("telescope.builtin")
+		local telescopeConfig = require("telescope.config")
+
+		-- Clone the default Telescope configuration
+		local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+		-- I want to search in hidden/dot files.
+		table.insert(vimgrep_arguments, "--hidden")
+		-- I don't want to search in the `.git` directory.
+		table.insert(vimgrep_arguments, "--glob")
+		table.insert(vimgrep_arguments, "!**/.git/*")
+
+		telescope.setup({
+			defaults = {
+				vimgrep_arguments = vimgrep_arguments,
+				file_ignore_patterns = { "%.git/" },
+				path_display = { "truncate " },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+					},
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
+		})
+
+		telescope.load_extension("fzf")
+
+		-- set keymaps
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy [f]ind [f]iles in cwd" })
+		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Fuzzy [f]ind [o] files" })
+		vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "[F]ind [s]tring in cwd" })
+		vim.keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "[F]ind string under [c]ursor in cwd" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind existing [b]uffers" })
+		vim.keymap.set(
+			"n",
+			"<leader>/",
+			builtin.current_buffer_fuzzy_find,
+			{ desc = "[/] Fuzzily search in current buffer" }
+		)
+		vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "[F]ind [G]it Files" })
+		vim.keymap.set("n", "<leader>fh", function()
+			builtin.help_tags({ previewer = false })
+		end, { desc = "[F]ind [H]elp" })
+		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
+		vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
+		vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "[F]ind [T]reesitter" })
+	end,
+}
