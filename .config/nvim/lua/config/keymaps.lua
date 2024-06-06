cb447d19fdb6231c0fd8587113ed7cd7a47b94d7
Dimitar Banev <banev_dimitar@mail.com>
2024-06-06 22:16:09 +0300

    disable arrow keys in neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set({"", "!"}, "<Right>", "", {})
+vim.keymap.set({"", "!"}, "<Left>", "", {})
+vim.keymap.set({"", "!"}, "<Up>", "", {})
+vim.keymap.set({"", "!"}, "<Down>", "", {})
