6b2bd0b48a13d75de1eb88367968e42f62389a85
Dimitar Banev <banev_dimitar@mail.com>
2024-07-16 21:33:36 +0300

    remove keymaps for lazyvim's terminal

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,4 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Enable / Disable autoformat
+vim.g.autoformat = false
+
 vim.g.lazyvim_picker = "fzf"
--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -8,9 +8,6 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
-      window = {
-        position = "float",
-      },
       filesystem = {
         filtered_items = {
           visible = true,
