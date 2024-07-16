83c3592570bb4013f7e6c906c262c920693819ad
Dimitar Banev <banev_dimitar@mail.com>
2024-07-17 01:51:15 +0300

    added edgy and fixed bug

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -8,6 +8,9 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        position = "float",
+      },
       filesystem = {
         filtered_items = {
           visible = true,
