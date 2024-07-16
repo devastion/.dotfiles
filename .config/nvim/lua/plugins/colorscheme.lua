4703ee87d7d9503018dddca96b3f62c06c210dbc
Dimitar Banev <banev_dimitar@mail.com>
2024-07-16 21:21:12 +0300

    readd lazyvim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    name = "onedark",
+    lazy = true,
+    opts = {
+      style = "darker",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
